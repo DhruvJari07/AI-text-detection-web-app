88e8b72c9d8eab6afa7390bdd53f3ab76f477b58
DhruvJari07 <jariwaladhruv@yahoo.com>
2024-03-19 13:45:16 +0530

    data_transform_main_setup

--- main.py
+++ main.py
@@ -13,14 +13,15 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
-    raise e
+    raise CustomException(e, sys)
 
 STAGE_NAME = "Data Transformation Stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataTransformationPipeline()
-    obj.main()
+    obj_transform = DataTransformationPipeline()
+    obj_transform.main()
+    X_train, y_train, X_test, y_test = obj_transform.X_train, obj_transform.y_train, obj_transform.X_test, obj_transform.y_test
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
