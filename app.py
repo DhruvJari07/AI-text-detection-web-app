96a854e28d0f7e12cc417b3cf5e4f375110f2a5b
DhruvJari07 <jariwaladhruv@yahoo.com>
2024-03-20 19:04:27 +0530

    Streamlit app added

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from AI_Text_Detection.pipeline.Predict_pipeline import PredictPipeline
+
+def main():
+    # Set title and description
+    st.title("AI & Human Text Classification")
+    st.write("To detect if your text is written by human or AI, enter your text below.")
+
+    # Text input
+    user_input = st.text_area("Enter your text here:", "")
+
+    # Predict button
+    if st.button("Predict"):
+        # Instantiate PredictPipeline
+        predictor = PredictPipeline()
+
+        # Make prediction
+        prediction = predictor.predict([user_input])
+
+        # Display prediction result
+        st.write(f"Prediction: Your text is {prediction} generated.")
+
+if __name__ == "__main__":
+    main()
---
+++ src/AI_Text_Detection/pipeline/Predict_pipeline.py
@@ -0,0 +1,17 @@
+import os
+from AI_Text_Detection.utils.common import load_object
+
+class PredictPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def predict(self, text):
+        model = load_object(os.path.join("artifacts","model_training","model.pkl"))
+        result = model.predict(text)
+        if result[0] == 0:
+            result = "Human"
+        else:
+            result = "AI"
+        
+        return result
+        
