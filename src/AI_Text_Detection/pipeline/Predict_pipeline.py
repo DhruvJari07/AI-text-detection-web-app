f8ab21e796e2b4eae8563f084ac427a530d53dce
DhruvJari07 <jariwaladhruv@yahoo.com>
2024-04-09 15:03:32 +0530

    dockerization

--- src/AI_Text_Detection/pipeline/Predict_pipeline.py
+++ src/AI_Text_Detection/pipeline/Predict_pipeline.py
@@ -6,7 +6,8 @@ class PredictPipeline:
         pass
 
     def predict(self, text):
-        model = load_object(os.path.join("artifacts","model_training","model.pkl"))
+        # model = load_object(os.path.join("artifacts","model_training","model.pkl"))
+        model = load_object(os.path.join("model","model.pkl"))
         result = model.predict(text)
         if result[0] == 0:
             result = "Human"
