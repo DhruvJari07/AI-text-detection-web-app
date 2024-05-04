009e669aa59808c028dbafe55a304eb82b5ec0ed
DhruvJari07 <jariwaladhruv@yahoo.com>
2024-05-04 16:00:59 +0530

    streamlit app clear button added

--- app.py
+++ app.py
@@ -8,20 +8,24 @@ load_dotenv()
 
 Lambda_api_url = os.getenv("LAMBDA_API_URL")
 
+def clear_text():
+    st.session_state["text"] = ""
+
 def main():
     # Set title and description
     st.title("AI & Human Text Classification")
     st.write("To detect if your text is written by human or AI, enter your text below.")
 
     # Text input
-    user_input = st.text_area("Enter your input text here:", "", height=300)
+    user_input = st.text_area("Enter your input text here:", "", height=250, key='text')
 
     # Model selection
     selected_model = st.selectbox("Select Model", ["Traditional ML Model", "Finetuned DistilBert Model"])
 
+    predict_col, clear_col, _, _, _, _, _ = st.columns([1,1,1,1,1,1,1], gap="small")
 
     # Predict button
-    if st.button("Predict"):
+    if predict_col.button("Predict", key="predict"):
         with st.spinner("Predicting..."):
                 if selected_model == "Traditional ML Model":
                         prediction = PredictPipeline().predict([user_input])
@@ -31,6 +35,7 @@ def main():
                 # Display prediction result
                 st.write(f"Prediction: Your text is **{prediction}** generated.")
 
+    clear_col.button("Clear", key="clear", on_click=clear_text)
 
 if __name__ == "__main__":
     main()
